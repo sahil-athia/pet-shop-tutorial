a26aab189b2cd8b1e1737303a69c74910100884f
Sahil Athia <sahilathia918@gmail.com>
2021-02-07 17:28:55 -0500

    add adopt function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
+  address[16] public adopters;
+  // this is an array of ETH addresses
+  // the address length in this case is 16
 
+  function adopt(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
+
+    adopters[petId] = msg.sender;
+    // here, we add the address to our array 
+    
+    return petId;
+  }
 }
