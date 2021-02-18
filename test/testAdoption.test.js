4c17b31e4d9d2c5c2a569101db853f0b306aeaa8
Sahil Athia <sahilathia918@gmail.com>
2021-02-18 14:49:46 -0500

    test the adopt function with js

--- test/testAdoption.test.js
+++ test/testAdoption.test.js
@@ -9,9 +9,16 @@ contract("Adoption", (accounts) => {
  });
 
  describe("adopting a pet and retrieving account addresses", async () => {
-   before("adopt a pet using accounts[0]", async () => {
-     await adoption.adopt(8, { from: accounts[0] });
-     expectedAdopter = accounts[0];
-   });
+    before("adopt a pet using accounts[0]", async () => {
+      await adoption.adopt(8, { from: accounts[0] });
+      expectedAdopter = accounts[0];
+    });
+
+    it("can fetch the address of the owner by petId", async () => {
+      const adopter = await adoption.adopters(8);
+
+      // trufle imports chai so we can use assertations
+      assert.equal(adopter, expectedAdopter, "The owner of the adopted pet should be the first account.")
+    })
  });
 });
